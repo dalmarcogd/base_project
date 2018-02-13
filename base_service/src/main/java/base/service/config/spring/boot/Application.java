6b28339b2f4dcca9a9fbc9ebe2a0dcadd4d61140
Guilherme Jacob Licinio Dalmarco <dalmarco.gd@gmail.com>
2018-02-13 20:22:50 -0200

    initital project

---
+++ base_service/src/main/java/base/service/config/spring/boot/Application.java
@@ -0,0 +1,10 @@
+package base.service.config.spring.boot;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// 
+
+	}
+
+}
---
+++ base_service/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * TODO doc me.
+ *
+ * @autor Guilherme Dalmarco (dalmarco.gd@gmail.com)
+ */
+module base.service {
+	requires java.base;
+//	requires org.springframework;
+}
